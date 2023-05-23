abc4a5bfc971196535e4681b731542fbb946330e
Micha?l HENKENS <Michael.HENKENS@nbb.be>
2023-05-23 09:03:02 +0200

    feat(config): initial configuration for eslint-config that match the config fo tslint that we have in code-style

---
+++ src/test/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	extends: ["../typescript"],
+	rules: {
+		"@angular-eslint/component-selector": "off",
+		"@angular-eslint/directive-selector": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"jsdoc/check-indentation": "off",
+		"jsdoc/newline-after-description": "off",
+		"no-useless-constructor": "error",
+		"@typescript-eslint/no-useless-constructor": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"prefer-const": "off"
+	}
+};
